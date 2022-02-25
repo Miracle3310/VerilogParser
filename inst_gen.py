52b75b2fde34e6b60e6d99b62055959f2f42d507
miracle3310 <miracle3310@qq.com>
2022-02-25 14:03:03 +0800

    port declaration

--- inst_gen.py
+++ inst_gen.py
@@ -26,22 +26,38 @@ def get_arg(options, option):
 
 def gen_module_instance(rtl: rtl_parser):
     ports_num = len(rtl.port_list)
+    index = 0
     line_list = []
-
     line_list.append('{} u_{}'.format(rtl.module_name, rtl.module_name))
     line_list.append('(')
-    index = 0
+
+    len_port_name = rtl.max_len_port_name
+    len_width = rtl.max_len_width
 
     for unit in rtl.port_list:
-        post_fix = ' //{:<6} width:{:<30} {}'.format(
-            unit['direction'], unit['width'], unit['comment'])
+        post_fix = ' //{:<6} width:{:<{}} {}'.format(
+            unit['direction'], unit['width'], len_width, unit['comment'])
         if index == ports_num - 1:
             post_fix = ' ' + post_fix
         else:
             post_fix = ',' + post_fix
-        line_list.append('\t.{:<{}}({:<{}}){:<30}'.format(
-            unit['name'], rtl.max_len_port_name, unit['name'], rtl.max_len_port_name, post_fix))
+        line_list.append('\t.{:<{}}({:<{}}){}'.format(
+            unit['name'], len_port_name, unit['name'], len_port_name, post_fix))
         index += 1
+    line_list.append(');')
+
+    for unit in line_list:
+        print(unit)
+
+
+def gen_port_declaration(rtl: rtl_parser):
+    line_list = []
+    len_port_name = rtl.max_len_port_name
+    len_width = rtl.max_len_width
+
+    for unit in rtl.port_list:
+        line_list.append('{:<4} {:<{}} {:<{}};'.format(
+            unit['type'], unit['width'], len_width, unit['name'], len_port_name))
     line_list.append(');')
 
     for unit in line_list:
@@ -67,6 +83,7 @@ if __name__ == "__main__":
 
     rtl = rtl_parser(file_name_arg, module_name)
     print('file name:{}\tmodule name:{}'.format(filename_we, rtl.module_name))
-    print(rtl.port_list)
+    # print(rtl.port_list)
     # print(rtl.max_len_port_name)
     gen_module_instance(rtl)
+    gen_port_declaration(rtl)
--- rtl_parser.py
+++ rtl_parser.py
@@ -8,6 +8,7 @@ class rtl_parser:
         self.extract_list = []  # exclude comment lines
         self.port_list = []  # name, direction, width, type, sign, comment
         self.max_len_port_name = 0
+        self.max_len_width = 0
 
         self.regex_module_head = re.compile(r'''
         (module\s+) # 1 keyword(module)
@@ -61,19 +62,21 @@ class rtl_parser:
                 port_direction = re_ports_obj.group(1)
                 port_type = re_ports_obj.group(3)
                 port_sign = re_ports_obj.group(5)
-                port_width_str = re_ports_obj.group(7)
+                port_width = re_ports_obj.group(7)
                 port_name = re_ports_obj.group(9)
                 port_comment = re_ports_obj.group(12)
-                if port_type == '':
+                if port_type is None:
                     port_type = 'reg' if (
                         port_direction == 'output') else 'wire'
-                if port_width_str == None:
-                    port_width_str = ''
+                if port_width is None:
+                    port_width = ''
                 if len(port_name) > self.max_len_port_name:
                     self.max_len_port_name = len(port_name)
+                if len(port_width) > self.max_len_width:
+                    self.max_len_width = len(port_width)
                 port_info = {'name': port_name,
                              'direction': port_direction,
-                             'width': port_width_str,
+                             'width': port_width,
                              'type': port_type,
                              'sign': port_sign,
                              'comment': port_comment}
